474adc1ec5db82d63144e5dbfa889717cbef269d
pedroslrn <pedrovslrn@gmail.com>
2024-07-28 17:17:38 -0300

    adicao dos testes da questao

---
+++ testeQuestao1526.py
@@ -0,0 +1,22 @@
+import unittest
+from Questao1526 import Solution
+
+class TestSolution(unittest.TestCase):
+    
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_minNumberOperations_1(self):
+        target = [1,2,3,2,1]
+        expected_operations = 3
+        self.assertEqual(self.solution.minNumberOperations(target), expected_operations)
+
+    def test_minNumberOperations_2(self):
+        target = [3, 1,1,2]
+        expected_operations = 4
+        self.assertEqual(self.solution.minNumberOperations(target), expected_operations)
+
+    
+
+if __name__ == "__main__":
+    unittest.main()
